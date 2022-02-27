47ddc8dc5b3d35c6e5cf53d260da0b562c903032
Zachary Sirna <zsirna@yahoo.com>
2022-02-27 18:12:32 -0500

    commit tree 14a1581f0eb57bf28b6af569b4ce2890fa8f907c

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    return cats.push("Ralph")
+} 
+
+function destructivelyPrependCat() { 
+    return cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+
+destructivelyRemoveFirstCat = () => cats.shift()
+
+function appendCat(){
+    return [...cats, "Broom"]
+}
+
+function prependCat(){
+    return ["Arnold", ...cats]
+}
+
+function removeLastCat(){
+    const newCatList = [...cats]
+    newCatList.pop()
+    return newCatList
+}
+
+function removeFirstCat(){
+    const newerCatList = [...cats]
+    newerCatList.shift()
+    return newerCatList
+}
+
